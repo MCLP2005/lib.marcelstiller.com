340816a0f9a923d07b1176ea9799e97d61f092cb
Eric Marcel Stiller <47244747+MCLP2005@users.noreply.github.com>
2025-08-26 12:25:45 +0000

    server list is now being processed in while using read

--- internal_scripts/sync/deploy-graphics.sh
+++ internal_scripts/sync/deploy-graphics.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
+
+# This script deploys the generated graphics to all servers listed in server.list
+
 serverlist=$(cat ~/server.list)
 repo="~/lib.marcelstiller.com/" # not used yet but might become usefull in the future
 
-for s in $serverlist; do
-    scp $s:/var/www/html/monitoring/graphics/local.svg /tmp/graphics/monitoring/$s.svg
+echo $serverlist | while IFS=';' read host name info; do 
+    scp $host:/var/www/html/monitoring/graphics/local.svg /tmp/graphics/monitoring/$name.svg # Copy local.svg from remote host to local tmp directory
 done
 
-for s in $serverlist; do
-    scp /tmp/graphics/monitoring/*.svg $s:/var/www/html/monitoring/graphics/
+echo $serverlist | while IFS=';' read host name info; do
+    scp /tmp/graphics/monitoring/*.svg $host:/var/www/html/monitoring/graphics/ # Copy all SVG files from local tmp directory to remote host
 done
 
