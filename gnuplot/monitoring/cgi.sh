757c4ab947e037fd44ef6ee79b7a69c24f842c79
Eric Marcel Stiller <47244747+MCLP2005@users.noreply.github.com>
2025-08-26 10:32:10 +0000

    Better version

--- gnuplot/monitoring/cgi.sh
+++ gnuplot/monitoring/cgi.sh
@@ -1,21 +1,25 @@
 #!/bin/bash
 
 logdir="/data/logs"
+output="/data/cgi.csv"
 
-deathscreen="$logdir/deathscreen.log"
-style="$logdir/style.log"
-teapot="$logdir/teapot.log"
+# List of log files (expandable)
+logfiles=("teapot.log" "deathscreen.log" "style.log")
 
-deathscreen=$(cat $deathscreen | uniq -c | sed 's/^ *//;s/ *$//;s/\t/_/g')
-style=$(cat $style | uniq -c | sed 's/^ *//;s/ *$//;s/\t/_/g')
-teapot=$(cat $teapot | uniq -c | sed 's/^ *//;s/ *$//;s/\t/_/g')
+# Get current timestamp
+timestamp=$(date -d "1 minute ago" +"%F %H:%M")
 
-echo $deathscreen | while IFS='_' read dt d ; do
-    echo $style | while IFS='_' read st s ; do
-        echo $teapot | while IFS='_' read tt t ; do
-            if test "$dt" = "$st" && test "$dt" = "$tt"; then
-                echo "$dt;$t;$d;$s" > /data/logs/cgi.log
-            fi
-        done
-    done
+# Initialize array with timestamp
+row=("$timestamp")
+
+# Loop through log files and count lines (assuming each line is a timestamp entry)
+for logfile in "${logfiles[@]}"; do
+    count=$(wc -l < "$logdir/$logfile")
+    row+=("$count")
 done
+
+# Output to CSV
+(
+    IFS=;
+    echo "${row[*]}"
+) >> "$output"
